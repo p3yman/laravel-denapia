6e94bb73e938ceedec00d57681831a62792383ad
Peyman Eskandari <peyman.eskandari@gmail.com>
2018-07-04 10:42:57 +0430

    Create service provider

---
+++ src/DenapiaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Peyman3d\Denapia;
+
+use Illuminate\Support\ServiceProvider;
+
+class DenapiaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
