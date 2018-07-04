f5dc0536c920872ecf3b0e6c12c844c61d9e5c55
Peyman Eskandari <peyman.eskandari@gmail.com>
2018-07-04 19:41:40 +0430

    update footer

--- resources/admin/partials/footer.blade.php
+++ resources/admin/partials/footer.blade.php
@@ -1,34 +1,12 @@
 <footer class="footer">
     <div class="container">
         <nav>
-            <ul class="footer-menu">
-                <li>
-                    <a href="#">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Company
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Portfolio
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Blog
-                    </a>
-                </li>
-            </ul>
             <p class="copyright text-center">
                 ©
                 <script>
                     document.write(new Date().getFullYear())
                 </script>
-                <a href="https://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+                <a href="https://github.com/peyman3d/laravel-denapia">Denapia</a>, made with <a href="https://laravel.com">Laravel</a> and ❤️
             </p>
         </nav>
     </div>
